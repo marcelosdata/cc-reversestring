465f161ba7bebd651e731b0785c3a5d02071034b
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-03-17 19:53:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function solution(str){
+ return  rev = str.split("").reverse().join("")
+   //en esta funcion primero ocupamaos split para dividir cada elemento por caracter en esta ocacion "" en array, luego reverse para revertir el sentido de cada elemento del array y por ultimo join hace que un array se convierta en string si usamos metodo ""
+}
+
+console.log(solution('marcelo'))
